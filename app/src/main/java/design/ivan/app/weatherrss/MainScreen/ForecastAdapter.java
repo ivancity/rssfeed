d01eaf25cfb6e5594e621ec04103e93eba9501e1
ivancity <shieldm@hotmail.com>
2016-07-15 03:40:17 +0300

    getting adapter and view holders ready for recycler view

--- app/src/main/java/design/ivan/app/weatherrss/MainScreen/ForecastAdapter.java
+++ app/src/main/java/design/ivan/app/weatherrss/MainScreen/ForecastAdapter.java
@@ -1,11 +1,17 @@
 package design.ivan.app.weatherrss.MainScreen;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayout;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import design.ivan.app.weatherrss.R;
 
 /**
  * Created by ivanm on 7/14/16.
@@ -16,14 +22,42 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
         void onClick(String date);
     }
 
-    public ForecastAdapter(Context context, ForecastAdapterOnClickHandler clickHandler){
+    private static final int VIEWTYPE_CURRENT = 0;
+    private static final int VIEWTYPE_GENERIC = 1;
 
+    Context context;
+    ForecastAdapterOnClickHandler clickHandler;
+
+    public ForecastAdapter(Context context, ForecastAdapterOnClickHandler clickHandler){
+        this.context = context;
+        this.clickHandler = clickHandler;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        return (position == 0) ? VIEWTYPE_CURRENT : VIEWTYPE_GENERIC;
+    }
 
     @Override
-    public ForecastViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ForecastViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        if ( viewGroup instanceof RecyclerView ) {
+            int layoutId = -1;
+            switch (viewType) {
+                case VIEWTYPE_CURRENT: {
+                    layoutId = R.layout.main_list_item_first;
+                    break;
+                }
+                case VIEWTYPE_GENERIC: {
+                    layoutId = R.layout.main_list_item_generic;
+                    break;
+                }
+            }
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(layoutId, viewGroup, false);
+            view.setFocusable(true);
+            return new ForecastViewHolder(view);
+        } else {
+            throw new RuntimeException("Something is wrong with the RecyclerView");
+        }
     }
 
     @Override
@@ -38,10 +72,33 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
 
     public class ForecastViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
+        @Nullable @BindView(R.id.item_date_title)
+        TextView dateTitle;
+        @Nullable @BindView(R.id.item_day)
+        GridLayout itemDay;
+        @Nullable @BindView(R.id.item_night)
+        GridLayout itemNight;
+        TextView tempMaxDay, tempMinDay;
+        TextView windMaxDay, windMinDay;
+        TextView tempMaxNight, tempMinNight;
+        TextView windMaxNight, windMinNight;
 
         public ForecastViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+            if(itemDay != null){
+                tempMaxDay = ButterKnife.findById(itemDay, R.id.main_list_item_max_temp);
+                tempMinDay = ButterKnife.findById(itemDay, R.id.main_list_item_min_temp);
+                windMaxDay = ButterKnife.findById(itemDay, R.id.main_list_item_max_wind);
+                windMinDay = ButterKnife.findById(itemDay, R.id.main_list_item_min_wind);
+            }
+            if (itemNight != null) {
+                tempMaxNight = ButterKnife.findById(itemNight, R.id.main_list_item_max_temp);
+                tempMinNight = ButterKnife.findById(itemNight, R.id.main_list_item_min_temp);
+                windMaxNight = ButterKnife.findById(itemNight, R.id.main_list_item_max_wind);
+                windMinNight = ButterKnife.findById(itemNight, R.id.main_list_item_min_wind);
+            }
+
 
         }
 
